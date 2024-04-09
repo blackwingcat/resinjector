5c5f0e374f6e85a999b56b9c5bce8844fa373e18
BlackWingCat <blackwingcat2000@gmail.com>
2024-04-09 10:19:02 +0900

    Update resinjector.c
    debugged

--- resinjector.c
+++ resinjector.c
@@ -23,7 +23,7 @@ typedef struct
     WORD wValueLength;
     WORD wType;
     wchar_t szKey[15];
-  	WORD Padding1;
+	WORD Padding1;
     VS_FIXEDFILEINFO Value;
     WORD Padding2;
     WORD Children;
@@ -32,9 +32,11 @@ typedef struct
 int main(int argc, char* argv[])
 {
 	int i;
+	int removeFlag = 0;
 	int *updatePointer = 0;
 	char AssemblyPath[640] = {0};
-	VS_VERSIONINFOA vi = {0};
+	char manifestPath[640] = {0};
+		VS_VERSIONINFOA vi = {0};
 	lstrcpyW(vi.szKey,L"VS_VERSION_INFO");
 	vi.Value.dwSignature = 0xfeef04bd;
 	vi.Value.dwStrucVersion = 0x00010000;
@@ -89,10 +91,25 @@ int main(int argc, char* argv[])
 		vi.Value.dwFileSubtype = 0;
 	vi.Value.dwFileDateMS = 0;
 	vi.Value.dwFileDateLS = 0;
-	printf("resource inector v0.1\n");
+	printf("resource inector v0.1 / (c)2024 BlackWingCat/PFW\n");
+	if(argc == 1) {
+		printf("usage: resinjector [exe/dll file] ... insert dummy version\n");
+		printf("usage: resinjector /m [manifestfile] [exe/dll file] ... insert manifest file\n");
+		printf("additional option: /r ... Remove other resource\n");
+		return 0;
+	}
 	for(i=1;i<argc;i++){
 		if(*argv[i]=='/' ||*argv[i]=='-'){
 			//option
+			if((argv[i][1]|' ') == 'm') {
+				if(i + 1 < argc) {
+					lstrcpyA(manifestPath,argv[i+1]);
+					i++;
+				}
+			} else if((argv[i][1]|' ') == 'r') {
+				removeFlag = 1;
+			}
+
 		} else {
 			lstrcpyA(AssemblyPath,argv[i]);
 		}
@@ -101,33 +118,54 @@ int main(int argc, char* argv[])
 		printf("no input file\n");
 		return 0;
 	}
-	updatePointer = (int *)BeginUpdateResource(AssemblyPath, false);
+	updatePointer = (int *)BeginUpdateResource(AssemblyPath, removeFlag);
 	BYTE *manifestByteArray = NULL;
 	bool result = false;
 	BYTE nullp [] = "";
 	manifestByteArray = nullp;
 	if (updatePointer == NULL) {
-		printf("can not open exe\n");
+		printf("can not open exe.\n");
 		return 0;
 	}
 	try{
 		int _langID = MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US);
-        if (UpdateResource(updatePointer, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), _langID, &vi, vi.wLength) != 1)
-//		if (UpdateResource(updatePointer, (const char *)24, (const char *)ResourceName, 0, manifestByteArray, mBALength != 1) )
-		{
-			printf("can not udate res\n");
+		unsigned int ResourceName = 1 ,mBALength = 1;
+		
+		if(*manifestPath){
+			FILE *fs;
+			char manifestData[4096];
+			fs = fopen(manifestPath,"r");
+			if(fs) {
+#define RT_MANIFESTID 1 
+				int mBALength =fread(manifestData,1,4096, fs);
+				fclose(fs);
+				printf("loaded manifest file %s[%d]\n",manifestPath,mBALength);
+				result = 
+				(bool)UpdateResource(updatePointer, (const char *)RT_MANIFEST, MAKEINTRESOURCE(RT_MANIFESTID), 0, manifestData, mBALength);
+			} else {
+				result = false;
+				printf("Could not load manifest file %s\n",manifestPath);
+				
+			}
+		} else {
+	        result = 
+				(bool)UpdateResource(updatePointer, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), _langID, &vi, vi.wLength);
+		}
+		if (result  != 1) {
+			printf("can not udate res[%d]\n",GetLastError());
 			return 0;
 		}
 	} catch (...) {
 		result =true;
 	}
 	if (updatePointer != 0) {
-		if(1 != EndUpdateResource(updatePointer, result)){
+		if(1 != EndUpdateResource(updatePointer, !result)){
 			printf("err.\n");
 		}
-		else printf("end.\n");
+		else printf("end[%s].\n",!result?"Discard":"Changed");
 	} else {
 			printf("can not finalize res\n");	
 	}
+
 	return 0;
 }
